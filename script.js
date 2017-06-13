c3a59508653d950d9a9f574fdd580f92b9e74ce7
Gord Bolli <gord@bolli.ca>
2017-06-12 23:43:53 -0700

    Added live animation

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
+
+function runTheClock() {
+
 // get hours, minutes, seconds from date
 var date = new Date();
 console.log(date);
@@ -20,3 +23,6 @@ HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
 SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
 
+};
+
+var interval = setInterval(runTheClock, 1000);
